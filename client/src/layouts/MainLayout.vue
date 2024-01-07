b7dbe47c59d776599cc45127454acc73392d363a
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-01-07 15:07:56 +0300

    init

---
+++ client/src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar class="column">
+        <div class="q-pa-md q-gutter-md">
+          <q-btn v-for="btn in buttons"
+          outline
+          :label="btn.label"/>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const buttons = ref ([
+  {
+    label: 'Mens',
+  },
+  {
+    label: 'New',
+  },
+  {
+    label: 'Womens',
+  },
+])
+</script>
